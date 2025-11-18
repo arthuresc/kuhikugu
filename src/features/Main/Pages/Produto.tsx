2e89f4ecdf1012979dbbf00f4a00f9331caa1bd5
Arthur Escalera <arthurescalera@gmail.com>
2025-11-18 12:51:03 -0300

    feat(Produto): Criada primeira versão da pagina de produtos
    Estou saindo pois quero criar uma variação do componente Carousel para ter quadros pequenos e assim usar na pagina de produtos além dos breadcrumbs

--- src/features/Main/Pages/Produto.tsx
+++ src/features/Main/Pages/Produto.tsx
@@ -1,7 +1,20 @@
 function Produto() {
   return (
-    <div>Produto</div>
-  )
+    <>
+      <div className="px-[10dvw] flex justify-around items-center text-neutral-900">
+        <img
+          className="max-h-[50dvh]"
+          src="https://placehold.co/1080x1350"
+          srcSet=""
+        />
+        <div className="px-[5dvw] py-[5dvh] *:mt-[1rem]">
+          <h1>Produto</h1>
+          <h2>Sub titulo do produto</h2>
+          <p>Commodo reprehenderit velit ullamco Lorem commodo eu. Magna sint duis eiusmod irure nulla adipisicing minim proident. Ipsum reprehenderit dolor officia consectetur. Occaecat quis pariatur consectetur commodo labore. Esse duis in consectetur pariatur minim fugiat. Pariatur sunt dolore sunt ea enim ea voluptate amet consectetur.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Produto
+export default Produto;
